d8d82bb616e370f12cb815feedf8470bfd43b02c
dolphin-vr <124384373+dolphin-vr@users.noreply.github.com>
2023-07-06 20:13:47 +0000

    Deploying to gh-pages from @ dolphin-vr/goit-js-hw-09@dc4feef3d1fa5c5e3d0f60b111ffbc0fd07420dc 🚀

---
+++ 01-color-switcher.beb17418.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")},n=null;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n)})),console.dir(t.body)}();
+//# sourceMappingURL=01-color-switcher.beb17418.js.map
