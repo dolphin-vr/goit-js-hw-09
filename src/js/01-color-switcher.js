5e295d52439c7ecf3a960065df034608bfe63bc5
dolphin <fun53@ukr.net>
2023-07-06 23:11:27 +0300

    t1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+   btnStart: document.querySelector('[data-start]'),
+   btnStop: document.querySelector('[data-stop]'),
+   body: document.querySelector('body'),
+}
+let colorId = null;
 
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+console.dir(refs.body)
+function onStart(){
+   refs.btnStart.disabled=true;
+   refs.btnStop.disabled=false;
+   colorId = setInterval(()=>{ refs.body.style.backgroundColor=getRandomHexColor()}, 1000)
+}
+
+function onStop(){
+   refs.btnStart.disabled=false;
+   refs.btnStop.disabled=true;
+   clearInterval(colorId)
+}
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
